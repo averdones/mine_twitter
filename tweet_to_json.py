2dc8375792c9a4ff2473c38fc185331c7ac13487
Antonio Verdone <averdones@gmail.com>
2017-10-20 00:35:17 +0200

    Add option to remove URL from text of tweet

--- tweet_to_json.py
+++ tweet_to_json.py
@@ -9,12 +9,13 @@ consumer_secret = "Consumer secret goes here"
 access_key = "access key goes here"
 access_secret = "access secret goes here"
 
-def tweet_to_json(tweet_id, write_json=True):
+def tweet_to_json(tweet_id, remove_url=True, write_json=True):
     """ Get json of a tweet.
 
     Inputs:
         - tweet_id: tweet identification number.
         - write_json: boolean. If True, writes the json file.
+        - remove_url: boolean. If True, remove url from the text of the tweet.
     """    
     # Authorize twitter, initialize tweepy
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -32,17 +33,38 @@ def tweet_to_json(tweet_id, write_json=True):
     # Save tweet
     tweet = api.get_status(tweet_id)
 
+    # Save jason as a string
+    json_str = json.dumps(tweet._json)
+
+    # Parse json from string
+    j = json.loads(json_str)
+
+    ### ----------------------------------------
+    # Remove the url from the text
+    if remove_url:
+        print("HOLA")
+        # Save url of the tweet
+        j_url = j["entities"]["media"][0]["url"]
+        
+        # Save text from the tweet
+        j_text = j["text"]
+        
+        # Remove url from text
+        j["text"] = j_text.replace(j_url, "")
+    ### ----------------------------------------
+        
     # Write json
     if write_json:
         with open(os.path.join(path_json, ("tweet_" + tweet_id + ".json")), "w") as f:
-            json.dump(tweet._json, f, sort_keys=True, indent=4)
+            json.dump(j, f, sort_keys=True, indent=4)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("tweet_id", help="Tweet id number")
+    parser.add_argument("remove_url", nargs="?", help="True to remove URL from text")
     parser.parse_args()
     args = parser.parse_args()
     try:
-        tweet_to_json(args.tweet_id)
+        tweet_to_json(args.tweet_id, int(args.remove_url))
     except:
         print("Oops! There is no tweet with this ID number.")
